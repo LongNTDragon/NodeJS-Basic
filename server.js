25f81e984144fb2be36945133727d1d3f9067f05
Admin <nguyenthanhlong19092002@gmail.com>
2023-06-02 13:33:15 +0700

    Hello World With NodeJS

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
-const http = require('http');
+const express = require('express')
+const app = express()
 
-const server = http.createServer((req, res) => {
-    console.log('run request ...')
-    res.setHeader('Content-Type', 'text/html');
-    res.write('Hello world !!!');
-    res.end();
+app.get('/', function (req, res) {
+    res.send('Hello World')
 })
 
-server.listen(3000, () => {
-    console.log('Node.JS server is running on port : 3000');
-})
+app.listen(3000)
